c3a01a3d3899cc66e3fdb2aa950a1719d10dc90f
tt <xialuoyite@gmail.com>
2020-05-12 11:27:49 +0800

    test

--- src/Test1.java
+++ src/Test1.java
@@ -3,5 +3,12 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("args = " + args);
         System.out.println("true = " + true);
+        System.out.println("true = " + true);
+        System.out.println("true = " + true);
+        System.out.println("true = " + true);
+        System.out.println("true = " + true);
+        System.out.println("true = " + true);
+        System.out.println("true = " + true);
+        System.out.println("true = " + true);
     }
 }
