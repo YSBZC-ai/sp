659958c6c5913fa2a0599f789f783373d7f56489
tt <xialuoyite@gmail.com>
2020-05-12 11:32:49 +0800

    test

--- src/Test1.java
+++ src/Test1.java
@@ -3,12 +3,6 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("args = " + args);
         System.out.println("true = " + true);
-        System.out.println("true = " + true);
-        System.out.println("true = " + true);
-        System.out.println("true = " + true);
-        System.out.println("true = " + true);
-        System.out.println("true = " + true);
-        System.out.println("true = " + true);
-        System.out.println("true = " + true);
+
     }
 }
